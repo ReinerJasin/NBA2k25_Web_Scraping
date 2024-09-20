9b7e63a661faaaa6f38bb21ac6f4bd5e01643d87
ReinerJasin <62686723+ReinerJasin@users.noreply.github.com>
2024-09-20 22:14:42 +0800

    Successfully scrape all html data from the URL
    next step is to find the specific data we want to extract

--- scraping_project/scraping.py
+++ scraping_project/scraping.py
@@ -1,9 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+import undetected_chromedriver as uc
 
 URL = "https://www.2kratings.com/"
-page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, "html.parser")
+driver = uc.Chrome()
+driver.get(URL)
+
+# page = requests.get(URL)
+
+page_source = driver.page_source
+soup = BeautifulSoup(page_source, "html.parser")
 
 print(soup)
