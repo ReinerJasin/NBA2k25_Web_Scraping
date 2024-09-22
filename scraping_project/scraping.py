a49620458319a0c017edc76d86f1cb0153c8d5dd
ReinerJasin <62686723+ReinerJasin@users.noreply.github.com>
2024-09-22 23:53:21 +0800

    Able to get bs4 element tag

--- scraping_project/scraping.py
+++ scraping_project/scraping.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import undetected_chromedriver as uc
 
-URL = "https://www.2kratings.com/"
+URL = "https://www.2kratings.com/current-teams"
 
 driver = uc.Chrome()
 driver.get(URL)
@@ -12,4 +12,13 @@ driver.get(URL)
 page_source = driver.page_source
 soup = BeautifulSoup(page_source, "html.parser")
 
-print(soup)
+# Get current teams and put in an array
+current_teams = soup.find_all("tr")
+
+for current_team in current_teams:
+    team_name = current_team.find("img", class_="mr-2")
+    print("=====DEBUG START=====")
+    print(type(team_name))
+    print("=====DEBUG END=====")
+    print(type(team_name))
+    # print(current_team, end="\n"*2)
