23f9ae1f79796b92eb2a9b243d9e600534a1df19
ReinerJasin <62686723+ReinerJasin@users.noreply.github.com>
2024-10-05 21:45:03 +0800

    Successfully access and get every player name from every team in current NBA teams

--- scraping_project/scraping.py
+++ scraping_project/scraping.py
@@ -19,23 +19,43 @@ soup = BeautifulSoup(page_source, "html.parser")
 
 # Create empty set to store all nba team names
 team_name_url = set()
+player_name_url = set()
 
 # Find all table row
 current_teams = soup.find_all("tr")
 
 # Extract the img tag and get the title of each team name
 for current_team in current_teams:
-    img_tag = current_team.find("img", class_="mr-2")
-    if img_tag:
-        team_name = img_tag['title']
-        team_name_url.add(team_name.lower().replace(" ", "-"))
+    # img_tag = current_team.find("img", class_="mr-2")
+    team_a_tag = current_team.find("a", href=True)
+    if team_a_tag:
+        team_name = team_a_tag.get('href')
+        team_name_url.add(team_name.replace("https://www.2kratings.com/teams/", ""))
+        # print(team_a_tag)
 
 # Print list of nba team names
 team_name_list = sorted(list(team_name_url))
 
+# Print all team_name_list that will be used as a url endpoint
+print(team_name_list)
+
 for team_name_url in team_name_list:
+# for team_name_url in ('atlanta-hawks', 'boston-celtics'):
     driver.get(BASE_URL + '/teams/' + team_name_url)
     page_source = driver.page_source
     soup = BeautifulSoup(page_source, "html.parser")
 
-    print(soup.title)
+    
+    current_players = soup.find("tbody")
+    # print(current_players)
+    for current_player in current_players:
+        player_a_tag = current_player.find("a", href=True)
+        if player_a_tag:
+            player_name = player_a_tag.get('href')
+            player_name_url.add(player_name.replace("https://www.2kratings.com/", ""))
+            # print(player_name)
+
+player_name_list = sorted(list(player_name_url))
+
+# Print all player_name_url that will be used as a url endpoint
+print(player_name_list)
