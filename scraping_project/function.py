87e213a771083c86fc821bd02c569503537a0304
ReinerJasin <62686723+ReinerJasin@users.noreply.github.com>
2024-10-06 21:53:37 +0800

    Create function.py for later

---
+++ scraping_project/function.py
@@ -0,0 +1,9 @@
+# SAVE FOR LATER 
+
+# from bs4 import BeautifulSoup
+
+# # Function to find a dataset from a team
+
+# # Function to find an entry of a specific player
+
+# # Function to get data from player 
